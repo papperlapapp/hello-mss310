a577ccad4829ea29a1004f5d1a64707bd6281225
Andreas Engel <papperlapapp@users.noreply.github.com>
2021-06-04 19:59:33 +0200

    Use a hardware timer for delay
    - Instead of using a very dumb delay routine (busy wait), use a less
      dumb one using one of the SoC's hardware timer. In all, the delay is
      still dumb, because it does not yet use an interrupt and instead polls
      the timer.
    - Switch to C++.

--- Makefile
+++ Makefile
@@ -2,10 +2,12 @@ CROSS_COMPILE = /opt/x-tools/mipsel-unknown-elf/bin/mipsel-unknown-elf-
 
 AS      = $(CROSS_COMPILE)as -mips32
 CC      = $(CROSS_COMPILE)gcc
+CXX     = $(CROSS_COMPILE)g++
 LD      = $(CROSS_COMPILE)ld
 OBJCOPY = $(CROSS_COMPILE)objcopy
 
-CFLAGS = -Os -DSTARTADDRESS=$(STARTADDRESS) -Wall -Wextra
+CFLAGS   = -Os -DSTARTADDRESS=$(STARTADDRESS) -Wall -Wextra -nostdlib
+CXXFLAGS = -std=c++17 -fno-rtti -fno-unwind-tables -fno-exceptions
 
 STARTADDRESS = 0x1000000
 
@@ -17,16 +19,20 @@ all: $(TARGETS)
 %.bin: %.elf
 	$(OBJCOPY) -O binary $< $@
 
+# FIXME: order of objects should not matter
 %.elf: start.o %.o
 	$(LD) -Ttext $(STARTADDRESS) -T linker.lds -Map $(@:.elf=.map) -o $@ $+
 
 %.o: %.[Sc]
 	$(CC) $(CFLAGS) -c -o $@ $<
 
+%.o: %.cpp
+	$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+
 .PHONY: clean
 clean:
 	rm -f *.o *.elf *.bin *~ *.map
 
-# ELF images can be used in radare2
+# ELF images can be used in cutter/radare2
 .PRECIOUS: %.elf
-	
+
--- blink.cpp
+++ blink.cpp
@@ -11,13 +11,34 @@
  * - resets the CPU when the # key is sent via the serial interface
  * - switches the relay off when the 0 key is pressed
  * - switches the relay on when the 1 key is pressed
+ *
+ * Please don't judge the code quality: its all just proof-of-concept code.
  */
 
-#include <stdint.h>
+#include <cstdint>
 
 #define RSTCTL          0x10000034      /* Reset control register */
 
 #define GPIO2_MODE      0x10000064      /* GPIO2 purpose selection */
+
+#define TGLB_REG        0x10000100      /* Timers global control register */
+
+#define TGBL_T0RST      (1 << 8)        /* Timer 0 reset */
+#define TGBL_T1RST      (1 << 10)       /* Timer 1 reset */
+
+#define T0CTL_REG       0x10000110      /* Timer 0 control register */
+#define T0LMT_REG       0x10000114      /* Timer 0 limit register */
+#define T0_REG          0x10000118      /* Timer 0 counter register */
+
+#define T1CTL_REG       0x10000130      /* Timer 1 control register */
+#define T1LMT_REG       0x10000134      /* Timer 1 limit register */
+#define T1_REG          0x10000138      /* Timer 1 counter register */
+
+#define TxCTL_Tx_PRES_OFFSET 16         /* Timer 0/1 prescale offset */
+#define TxCTL_Tx_PRES_MASK   0xFFFF     /* Timer 0/1 prescale mask */
+#define TxCTL_TxEN           (1 << 7)   /* Timer 0/1 enable */
+#define TxCTL_TxAL           (1 << 4)   /* Timer 0/1 auto-load enable */
+#define TxCTL_TxAL_STATUS    (1 << 3)   /* Timer 0/1 auto load enable status */
 
 #define GPIO_CTRL_1     0x10000604      /* Direction control register */
 #define GPIO_DATA_1     0x10000624      /* Data register              */
@@ -40,16 +61,16 @@
 #define GPIO_BUTTON        (1 << 3)
 
 
-static inline void write_l(unsigned int addr, unsigned int val)
+static inline void write_l(uint32_t addr, unsigned int val)
 {
-    volatile uint32_t *ptr = (uint32_t *)(addr);
+    volatile auto* ptr = reinterpret_cast<uint32_t *>(addr);
     *ptr = val;
 }
 
 
-static inline unsigned int read_l(unsigned int addr)
+static inline unsigned int read_l(uint32_t addr)
 {
-    volatile uint32_t *ptr = (uint32_t *)(addr);
+    volatile auto* ptr = reinterpret_cast<uint32_t *>(addr);
     return *ptr;
 }
 
@@ -73,7 +94,7 @@ static void serial_input()
 {
     if (read_l(UART0_LSR) & UART_LSR_DR) {
         /* Something in the RX buffer */
-        char received = read_l(UART0_RBR) & 0xFF;
+        auto received = read_l(UART0_RBR) & 0xFF;
         switch (received) {
         case '#':
             reset_cpu();
@@ -88,16 +109,39 @@ static void serial_input()
     }
 }
 
-/* A very dumb delay routine */
+/*
+ * Delay using timer 0 for exact timeout.
+ *
+ * Still a dumb delay, but this time with exact timeout.
+ */
+static void timer_delay(unsigned int delay_in_ms)
+{
+    // start by disabling timer
+    write_l(T0CTL_REG, 0);
+    write_l(TGLB_REG, 0);
+
+    write_l(T0LMT_REG, delay_in_ms);
+
+    // enable the timer, set prescale to 1ms
+    write_l(T0CTL_REG, (1000 << TxCTL_Tx_PRES_OFFSET) | TxCTL_TxEN);
+
+    // Busy wait until timer expires.
+    while ((read_l(T0CTL_REG) & TxCTL_TxEN) != 0) {
+        // sleep
+    }
+}
+
+
+/* A dumb delay routine */
 static void delay()
 {
-    volatile int i, j;
-    for (i = 0; i < 500; i++) {
+    const auto milliseconds = 500;
+    const auto granularity = 100;
+    static_assert(milliseconds % granularity == 0, "milliseconds should be a multiple of granularity");
+    for (auto i = 0; i < (milliseconds / granularity); i++) {
         reset_if_button_pressed();
         serial_input();
-        for (j = 0; j < 500; j++) {
-            /* Do nothing */
-        }
+        timer_delay(granularity);
     }
 }
 
@@ -105,9 +149,7 @@ static void delay()
 /* Send the given string to the UART */
 static void print_string(const char *s)
 {
-    const char *p;
-
-    for (p = s; *p; ++p) {
+    for (auto* p = s; *p; ++p) {
         while ((read_l(UART0_LSR) & UART_LSR_THRE) == 0) {
             /* Wait until there's some space in the FIFO */
         }
@@ -115,9 +157,7 @@ static void print_string(const char *s)
     }
 }
 
-#ifdef __cplusplus
 extern "C"
-#endif
 void entrypoint(void)
 {
     write_l(GPIO2_MODE, 0x05550550);  /* Value determined from original app */
